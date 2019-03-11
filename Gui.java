b48f762648c928471f9965286396c6ba1bf6af8f
bsheehan2121 <47328432+bsheehan2121@users.noreply.github.com>
2019-03-11 19:57:18 -0400

    supports up to 4 players with exception handling

--- Gui.java
+++ Gui.java
@@ -15,6 +15,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -41,6 +42,7 @@ import javafx.util.Duration;
 public class Gui extends Application{
     boolean raceing;
     ArrayList<DrawCar> cars;
+    Rectangle[] draw;
     
     Stopwatch s;
     Label time;
@@ -55,11 +57,12 @@ public class Gui extends Application{
         time = new Label("00:00");
         timer= new StopwatchGUI(s,time);
         cars = new ArrayList<DrawCar>();
+        draw = new Rectangle[4];
         //***************for testing perpouses*********************
-        cars.add(new DrawCar());
-        cars.add(new DrawCar());
-        cars.add(new DrawCar());
-        cars.add(new DrawCar());
+       // cars.add(new DrawCar());
+       // cars.add(new DrawCar());
+       // cars.add(new DrawCar());
+       // cars.add(new DrawCar());
         
         /**
          * ************Start Screen********************
@@ -69,136 +72,171 @@ public class Gui extends Application{
         display.setMinSize(200, 200);
         Button start = new Button("Start");
         Button addPlayer = new Button("Add Player");
-        
+        /**
+         * adds a car 
+         */
         addPlayer.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent e){
-                cars.add(new DrawCar());
-                String s = "";
-                for(DrawCar c: cars){
-                    s+= c.toString()+"\n";
+                if(cars.size()==4){
+                    Alert tooManyCars = new Alert(Alert.AlertType.WARNING);
+                    tooManyCars.setHeaderText("Too Many Cars");
+                    tooManyCars.setContentText("Already have max number of racers");
+                    tooManyCars.showAndWait();
+                }else{
+                    cars.add(new DrawCar());
+                    String s = "";
+                    for(DrawCar c: cars){
+                        s+= c.toString()+"\n";
+                    }
+                    display.setText(s);
                 }
-                display.setText(s);
             }
         });
-        
+        /**
+         * starts the main race
+         */
         start.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent e){
-                /**
-                 * *********Race Screen*************
-                 */
-                Pane secondaryLayout = new Pane();
-                
-                VBox v = new VBox();
-                
-                HBox h1 = new HBox();
-                HBox h2 = new HBox();
-                HBox h3 = new HBox();
-                //****************Header***************************************************************************
-                h1.setMinSize(500,100);
-                s.startTime();
-                timer.start();
-                h1.getChildren().addAll(time);
-                //***************Center***************************************************************************
-                
-                h2.setPrefSize(500, 500);
-                Pane race = new Pane();
-                race.setBackground(new Background(new BackgroundFill(
-                 Color.BURLYWOOD, CornerRadii.EMPTY, Insets.EMPTY)));
-                race.setMinSize(1200, 500);
-                
-                Ellipse track = new Ellipse(400,250,350,230);
-                track.setFill(Color.DARKGRAY);
-                
-                Button b = new Button("test");
-                
-                Ellipse lines = new Ellipse(400,250,325,205);
-                lines.getStrokeDashArray().addAll(25d, 10d);
-                lines.setFill(Color.DARKGRAY);
-                lines.setStroke(Color.BLACK);
-                
-                Ellipse lines2 = new Ellipse(400,250,300,180);
-                lines2.getStrokeDashArray().addAll(25d, 10d);
-                lines2.setFill(Color.DARKGRAY);
-                lines2.setStroke(Color.BLACK);
-                
-                Ellipse lines3 = new Ellipse(400,250,275,155);
-                lines3.getStrokeDashArray().addAll(25d, 10d);
-                lines3.setFill(Color.DARKGRAY);
-                lines3.setStroke(Color.BLACK);
+                if(cars.isEmpty()){
+                    Alert noCars = new Alert(Alert.AlertType.WARNING);
+                    noCars.setHeaderText("No Cars");
+                    noCars.setContentText("Please add atleast 1 car to race");
+                    noCars.showAndWait();
+                }else{
                 
                 
-                Ellipse track2 = new Ellipse(400,250,250,130);
+                    /**
+                    * ********************Race Screen*********************************************************************
+                    */
+                    Pane secondaryLayout = new Pane();
                 
-                Rectangle car1= cars.get(0).getDraw();
-                Rectangle car2= cars.get(1).getDraw();
-                Rectangle car3= cars.get(2).getDraw();
-                Rectangle car4= cars.get(3).getDraw();
+                    VBox v = new VBox();
                 
-                   //**************animation*************************************
+                    HBox h1 = new HBox();
+                    HBox h2 = new HBox();
+                    HBox h3 = new HBox();
+                    //****************Header***************************************************************************
+                    h1.setMinSize(500,100);
+                    s.startTime();
+                    timer.start();
+                    h1.getChildren().addAll(time);
+                    //***************Center***************************************************************************
                 
-                PathTransition t1 = new PathTransition();
-                PathTransition t2 = new PathTransition();
-                PathTransition t3 = new PathTransition();
-                PathTransition t4 = new PathTransition();
-                Ellipse car1path = new Ellipse(400,250,338,218);
-                Ellipse car2path = new Ellipse(400,250,313,193);
-                Ellipse car3path = new Ellipse(400,250,288,168);
-                Ellipse car4path = new Ellipse(400,250,263,143);
-                t1.setNode(car1);
-                t2.setNode(car2);
-                t3.setNode(car3);
-                t4.setNode(car4);
+                    h2.setPrefSize(500, 500);
+                    Pane race = new Pane();
+                    race.setBackground(new Background(new BackgroundFill(
+                    Color.BURLYWOOD, CornerRadii.EMPTY, Insets.EMPTY)));
+                    race.setMinSize(1200, 500);
                 
-                t2.setPath(car2path);
-                t2.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-                t2.setDuration(Duration.seconds(10));
-                t2.play();
+                    Ellipse track = new Ellipse(400,250,350,230);
+                    track.setFill(Color.DARKGRAY);
                 
-                t3.setPath(car3path);
-                t3.setDuration(Duration.seconds(10));
-                t3.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-                t3.play();
+                    Button b = new Button("test");
                 
-                t4.setPath(car4path);
-                t4.setDuration(Duration.seconds(10));
-                t4.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-                t4.play();
+                    Ellipse lines = new Ellipse(400,250,325,205);
+                    lines.getStrokeDashArray().addAll(25d, 10d);
+                    lines.setFill(Color.DARKGRAY);
+                    lines.setStroke(Color.BLACK);
                 
-                t1.setPath(car1path);
-                t1.setDuration(Duration.seconds(10));
-                t1.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-                t1.play();
-                race.getChildren().addAll(track,lines,lines2,lines3,track2,car1,car2,car3,car4);
-                h2.getChildren().add(race);
+                    Ellipse lines2 = new Ellipse(400,250,300,180);
+                    lines2.getStrokeDashArray().addAll(25d, 10d);
+                    lines2.setFill(Color.DARKGRAY);
+                    lines2.setStroke(Color.BLACK);
+                
+                    Ellipse lines3 = new Ellipse(400,250,275,155);
+                    lines3.getStrokeDashArray().addAll(25d, 10d);
+                    lines3.setFill(Color.DARKGRAY);
+                    lines3.setStroke(Color.BLACK);
+                
+                
+                    Ellipse track2 = new Ellipse(400,250,250,130);
+                
+                    // ***********Draw Cars******************************************************************************************
+                    if(cars.size()==4){
+                        draw[0] = cars.get(0).getDraw();
+                        draw[1] = cars.get(1).getDraw();
+                        draw[2] = cars.get(2).getDraw();
+                        draw[3] = cars.get(3).getDraw();
+                    }else if(cars.size()==3){
+                        draw[0] = cars.get(0).getDraw();
+                        draw[1] = cars.get(1).getDraw();
+                        draw[2] = cars.get(2).getDraw();
+                        draw[3] = new Rectangle(0,0);
+                    
+                    }else if(cars.size()==2){
+                        draw[0] = cars.get(0).getDraw();
+                        draw[1] = cars.get(1).getDraw();
+                        draw[2] = new Rectangle(0, 0);
+                        draw[3] = new Rectangle(0, 0);
+                    }else{
+                        draw[0] = cars.get(0).getDraw();
+                        draw[1] = new Rectangle(0, 0);
+                        draw[2] = new Rectangle(0, 0);
+                        draw[3] = new Rectangle(0, 0);
+                    }
+                   //***********************************animation*************************************************
+                
+                    PathTransition t1 = new PathTransition();
+                    PathTransition t2 = new PathTransition();
+                    PathTransition t3 = new PathTransition();
+                    PathTransition t4 = new PathTransition();
+                    Ellipse car1path = new Ellipse(400,250,338,218);
+                    Ellipse car2path = new Ellipse(400,250,313,193);
+                    Ellipse car3path = new Ellipse(400,250,288,168);
+                    Ellipse car4path = new Ellipse(400,250,263,143);
+                    t1.setNode(draw[0]);
+                    t2.setNode(draw[1]);
+                    t3.setNode(draw[2]);
+                    t4.setNode(draw[3]);
+                
+                    t2.setPath(car2path);
+                    t2.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+                    t2.setDuration(Duration.seconds(10));
+                    t2.play();
+                
+                    t3.setPath(car3path);
+                    t3.setDuration(Duration.seconds(10));
+                    t3.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+                    t3.play();
+                
+                    t4.setPath(car4path);
+                    t4.setDuration(Duration.seconds(10));
+                    t4.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+                    t4.play();
+                
+                    t1.setPath(car1path);
+                    t1.setDuration(Duration.seconds(10));
+                    t1.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+                    t1.play();
+                    race.getChildren().addAll(track,lines,lines2,lines3,track2,draw[0],draw[1],draw[2],draw[3]);
+                    h2.getChildren().add(race);
                 
                 //***************Bottom*****************************************************************************
                 
-                h3.setMinSize(200, 200);
+                    h3.setMinSize(200, 200);
                 
-                h3.setPadding(new Insets(15, 12, 15, 12));
-                h3.setSpacing(10);
-                //Pane p = new Pane();
-                //p.setMinSize(200, 200);
-                Button pause = new Button("Pause");
-                Button play = new Button("Play");
-                Button restart = new Button("Restart");
+                    h3.setPadding(new Insets(15, 12, 15, 12));
+                    h3.setSpacing(10);
+                    //Pane p = new Pane();
+                    //p.setMinSize(200, 200);
+                    Button pause = new Button("Pause");
+                    Button play = new Button("Play");
+                    Button restart = new Button("Restart");
                 
-                //p.getChildren().addAll(pause,play,restart);
-                h3.getChildren().addAll(pause,play,restart);
+                    //p.getChildren().addAll(pause,play,restart);
+                    h3.getChildren().addAll(pause,play,restart);
                 //**********************************************************************************************************8
-                v.getChildren().addAll(h1,h2,h3);
-                secondaryLayout.getChildren().addAll(v);
-                Scene secondScene = new Scene(secondaryLayout, 1200, 700);
-                Stage secondStage = new Stage();
-                secondStage.setTitle("Race");
-                secondStage.setScene(secondScene);
-                secondStage.show();
-                primaryStage.close();
-                
-                
-                
+                    v.getChildren().addAll(h1,h2,h3);
+                    secondaryLayout.getChildren().addAll(v);
+                    Scene secondScene = new Scene(secondaryLayout, 1200, 700);
+                    Stage secondStage = new Stage();
+                    secondStage.setTitle("Race");
+                    secondStage.setScene(secondScene);
+                    secondStage.show();
+                    primaryStage.close();       
+                }   
             }  
         });
         
